ec3bff5682a57ad041d60ce1986fe5782213b959
Dylan Wettlaufer <dylanwett11@gmail.com>
2025-07-31 15:09:52 -0400

    added login and signup frontend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from features.auth.auth import router as auth_router
 from utils.supabase_client import supabase
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include auth routes with /auth prefix
 app.include_router(auth_router, prefix="/auth")
